c38e8880db561f3dbaa89243a536d44ffc54bf73
KPkamil <kamil.pawlak51@gmail.com>
2025-08-05 11:41:25 +0200

    Create dashboard navbar, improve sidebar responsiveness, change font to inter

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -2,19 +2,32 @@ import { useRouter } from "next/navigation";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 
 import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import {
   DropdownMenu,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuContent,
   DropdownMenuTrigger,
   DropdownMenuSeparator,
-  DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 export const DashboardUserButton = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const { data, isPending } = authClient.useSession();
 
   const onLogout = () => {
@@ -29,6 +42,47 @@ export const DashboardUserButton = () => {
 
   if (isPending || !data?.user) {
     return null;
+  }
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="animate-in fade-in duration-1000 rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+          {data.user.image ? (
+            <Avatar className="size-9 mr-3">
+              <AvatarImage src={data.user.image} />
+            </Avatar>
+          ) : (
+            <GeneratedAvatar
+              variant="initials"
+              seed={data.user.name}
+              className="size-9 mr-3"
+            />
+          )}
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+            <p className="text-sm truncate w-full">{data.user.name}</p>
+            <p className="text-xs truncate w-full">{data.user.email}</p>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerDescription>{data.user.email}</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button onClick={() => {}} variant="outline">
+              <CreditCardIcon className="size-4 text-black" />
+              Billing
+            </Button>
+            <Button onClick={onLogout} variant="outline">
+              <LogOutIcon className="size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
   }
 
   return (
