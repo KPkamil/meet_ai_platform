a20065b2f72c9f405e5a848930f83f57206bec0b
KPkamil <kamil.pawlak51@gmail.com>
2025-08-06 19:47:04 +0200

    Setup agents

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -80,3 +80,16 @@ export const account = pgTable(
     userProviderUnique: unique().on(table.userId, table.providerId),
   })
 );
+
+// Agents
+
+export const agents = pgTable("agents", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  instructions: text("instructions").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
